6066bc62e001c21994fd3674571da1489353fcac
Victor Tran <victor-likes-coding@tempestas.io>
2023-01-31 23:24:51 -0500

    made some changes with event handler, not really graded assignment so not taking too long on this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+console.log("Hooked");
+
+// hover effect changing color of text
+const h1 = document.querySelector(".logo-heading");
+let timeoutId = null;
+
+const getRandomColorValue = () => {
+    return Math.floor(Math.random() * 255);
+};
+
+h1.addEventListener("mouseenter", (_) => {
+    timeoutId = setInterval(() => {
+        // every 1 second change the color of the text
+        const red = getRandomColorValue();
+        const green = getRandomColorValue();
+        const blue = getRandomColorValue();
+        const color = `rgb(${red}, ${green}, ${blue})`;
+        h1.setAttribute("style", `color: ${color}; cursor: pointer`);
+    }, 500);
+});
+
+h1.addEventListener("mouseout", (_) => {
+    if (timeoutId) {
+        clearInterval(timeoutId);
+    }
+});
+
+h1.addEventListener("click", (_) => {
+    if (timeoutId) {
+        clearInterval(timeoutId);
+    }
+});
+
+// const contact = document.querySelector("nav .nav-link:nth-child(4)");
+
+// const createModel = () => {
+//     const modal = document.createElement("div");
+//     const h2 = document.createElement("h2");
+//     const pEmail = document.createElement("p");
+//     const pPhone = document.createElement("p");
+//     h2.textContent = "Contact me";
+//     pEmail.textContent = "Email: contactme@spam.com";
+//     pPhone.textContent = "Phone: 1 (888) 666-1738";
+//     modal.appendChild(h2);
+//     modal.appendChild(pEmail);
+//     modal.appendChild(pPhone);
+
+//     return modal;
+// };
